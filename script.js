9e427005153f5c8dcf652818ad636a02a7d1c2ff
Panda <kenken@ab.cyberhome.ne.jp>
2026-08-01 14:32:50 +0900

    Re-add updateDate column in table
    script.：新体系統合-45
    微修正

--- script.js
+++ script.js
@@ -3112,10 +3112,6 @@ function buildPlayerRowHTML(
           ${shortShop}
         </div>
       </td>
-      
-      <td class="left">
-        ${p.updateDate}
-      </td>
 
       <td class="center">
         ${
@@ -3123,6 +3119,10 @@ function buildPlayerRowHTML(
             ? `<img src="${titleUrl}" height="24">`
             : ""
         }
+      </td>
+
+      <td class="left">
+        ${p.updateDate}
       </td>
     </tr>
   `;
